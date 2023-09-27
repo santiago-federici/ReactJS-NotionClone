9cb14f7a46ed33ae6d3b4ffe812649732dd2571e
santiago-federici <santiagofederici88@gmail.com>
2023-09-27 12:15:45 -0300

    Added priority options, styles, and functionalities

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,27 +1,35 @@
 import { useState } from 'react'
 import { GoDotFill } from 'react-icons/go'
 import { TodoOptions } from './TodoOptions'
+import { PriorityOptions } from './PriorityOptions'
 
 export function TaskItem({ taskName, taskStatus, due, priority }) {
+  // handling status text, style and options
   const [openTodoOptions, setOpenTodoOptions] = useState(false)
-
-  // handling status text
   const [statusInnerText, setStatusInnerText] = useState(taskStatus)
-  // handling status className
   const [statusClassName, setStatusClassName] = useState('')
+
+  // handling priority text, style and options
+  const [openProprityOptions, setOpenProprityOptions] = useState(false)
+  const [priorityInnerText, setPriorityInnerText] = useState(priority)
+  const [priorityClassName, setPriorityClassName] = useState('')
 
   return (
     <>
       <p>{taskName}</p>
       <p className='status-p' onClick={() => setOpenTodoOptions(!openTodoOptions)}>
-        <span className={`table-content__status ' + ${statusClassName}`}>
+        <span className={`status ' + ${statusClassName}`}>
           <GoDotFill />
           {statusInnerText}
         </span>
         {openTodoOptions && <TodoOptions setStatusInnerText={setStatusInnerText} setStatusClassName={setStatusClassName} />}
       </p>
       <p>{due}</p>
-      <p>{priority}</p>
+      <p className='priority-p' onClick={() => setOpenProprityOptions(!openProprityOptions)}>
+        <span className={`priority ' + ${priorityClassName}`}>
+          {priorityInnerText}
+          {openProprityOptions && <PriorityOptions setPriorityInnerText={setPriorityInnerText} setPriorityClassName={setPriorityClassName} />}
+        </span></p>
     </>
   )
 }
--- src/components/TodoOptions.jsx
+++ src/components/TodoOptions.jsx
@@ -3,9 +3,38 @@ import { GoDotFill } from 'react-icons/go'
 export function TodoOptions({ setStatusInnerText, setStatusClassName }) {
   return (
     <ul className='status-options'>
-      <li onClick={() => { setStatusInnerText('In Progress'); setStatusClassName('blue-status') }}><span className="table-content__status blue-status"><GoDotFill />In Progress</span></li>
-      <li onClick={() => { setStatusInnerText('Done'); setStatusClassName('green-status') }}><span className="table-content__status green-status"><GoDotFill />Done</span></li>
-      <li onClick={() => { setStatusInnerText('To Do'); setStatusClassName('') }}><span className="table-content__status"><GoDotFill />To Do</span></li>
+      <li
+        onClick={() => {
+          setStatusInnerText('In Progress')
+          setStatusClassName('blue-status')
+        }}>
+        <span className="status blue-status">
+          <GoDotFill />
+          In Progress
+        </span>
+      </li>
+
+      <li
+        onClick={() => {
+          setStatusInnerText('Done')
+          setStatusClassName('green-status')
+        }}>
+        <span className="status green-status">
+          <GoDotFill />
+          Done
+        </span>
+      </li>
+
+      <li
+        onClick={() => {
+          setStatusInnerText('To Do')
+          setStatusClassName('gray-status')
+        }}>
+        <span className="status gray-status">
+          <GoDotFill />
+          To Do
+        </span>
+      </li>
     </ul>
   )
 }
