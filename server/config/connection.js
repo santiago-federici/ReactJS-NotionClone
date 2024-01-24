12b474a5769faa663dfcb7c669e68641b3bbed0b
santiago-federici <santiagofederici88@gmail.com>
2024-01-24 20:49:58 -0300

    Modified env variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,17 @@
 import mysql from 'mysql2/promise'
 
+const DB_HOST = process.env.DB_HOST ?? 'localhost'
+const DB_USER = process.env.DB_USER ?? 'root'
+const DB_PORT = process.env.DB_PORT ?? 3306
+const DB_PASSWORD = process.env.DB_PASSWORD ?? 'admin'
+const DB_NAME = process.env.DB_NAME ?? 'notionclone_db'
+
 const config = {
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: 'admin',
-  database: 'notionclone_db'
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME
 }
 
 export const connection = await mysql.createConnection(config)
