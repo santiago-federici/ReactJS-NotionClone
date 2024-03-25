0109043eeb40d8e5299c3f340222a7a9a4fe1c10
santiago-federici <santiagofederici88@gmail.com>
2024-03-25 08:11:14 -0300

    fix cors issue

--- server/app.js
+++ server/app.js
@@ -12,46 +12,26 @@ export const createApp = ({ userModel, tableModel, rowModel, authModel }) => {
 
   app.use(corsMiddleware())
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', true)
-    next()
-  })
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Credentials', true)
+  //   next()
+  // })
 
-  app.options('*', corsMiddleware())
+  // app.options('*', corsMiddleware())
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Expose-Headers', 'Content-Type, Authorization')
-    next()
-  })
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Expose-Headers', 'Content-Type, Authorization')
+  //   next()
+  // })
 
   app.use(json())
   app.use(cookieParser())
   app.disable('x-powered-by')
 
-  app.use('/users', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-  }, createUsersRouter({ userModel }))
-  app.use('/tables', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-  }, createTablesRouter({ tableModel }))
-  app.use('/rows', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-  }, createRowsRouter({ rowModel }))
-  app.use('/auth', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-  }, createAuthRouter({ authModel }))
+  app.use('/users', createUsersRouter({ userModel }))
+  app.use('/tables', createTablesRouter({ tableModel }))
+  app.use('/rows', createRowsRouter({ rowModel }))
+  app.use('/auth', createAuthRouter({ authModel }))
 
   const PORT = process.env.PORT || 3000
 
